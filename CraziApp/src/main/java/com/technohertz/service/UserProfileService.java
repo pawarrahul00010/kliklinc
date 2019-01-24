a1012548106dfcd1cb0392e270e6dce1b0fe2c96
Administrator <admin@technohertz.com>
2019-01-24 17:08:43 +0530

    #combined both work

--- CraziApp/src/main/java/com/technohertz/service/UserProfileService.java
+++
@@ -1,9 +0,0 @@
-package com.technohertz.service;
-
-import java.util.List;
-
-import com.technohertz.model.UserRegister;
-
-public interface UserProfileService {
-	List<UserRegister> findByUserName(String userName);
-}
